feac6491047cc41c679553d4828e15ab330de80c
Aaryan <aaryan.mishra@gmail.com>
2023-09-30 01:44:47 +0530

    created backend

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 app.use(requestLogger)
+app.use(cors())
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
